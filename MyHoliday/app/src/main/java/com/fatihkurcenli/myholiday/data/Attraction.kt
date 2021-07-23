937b72d240b4987ef631d2c0529093f3f5411afb
Fatih <rizelifatih5353@gmail.com>
2021-07-23 19:19:29 +0300

    added viewholder and recyclerview

---
+++ MyHoliday/app/src/main/java/com/fatihkurcenli/myholiday/data/Attraction.kt
@@ -0,0 +1,16 @@
+package com.fatihkurcenli.myholiday.data
+
+data class Attraction(
+    val description: String = "",
+    val facts: List<String> = listOf(),
+    val id: String = "",
+    val image_urls: List<String> = listOf(),
+    val location: Location = Location(),
+    val months_to_visit: String = "",
+    val title: String = ""
+) {
+    data class Location(
+        val latitude: String = "",
+        val longitude: String = ""
+    )
+}
