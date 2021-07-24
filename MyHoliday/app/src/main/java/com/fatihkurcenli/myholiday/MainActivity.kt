48cae6dd11311dedb95995536b7f947e6488aa92
Fatih <rizelifatih5353@gmail.com>
2021-07-24 17:15:21 +0300

    added location ıntent

--- MyHoliday/app/src/main/java/com/fatihkurcenli/myholiday/MainActivity.kt
+++ MyHoliday/app/src/main/java/com/fatihkurcenli/myholiday/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.fatihkurcenli.myholiday.data.Attraction
 import com.fatihkurcenli.myholiday.data.AttractionResponse
 import com.squareup.moshi.JsonAdapter
@@ -12,6 +15,7 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 class MainActivity : AppCompatActivity() {
     lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
     val attractionList: List<Attraction> by lazy {
         parseAttractions()
@@ -25,7 +29,14 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         navController = navHostFragment.navController
 
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
     private fun parseAttractions(): List<Attraction> {
--- MyHoliday/app/src/main/java/com/fatihkurcenli/myholiday/ui/fragment/AttractionDetailFragment.kt
+++ MyHoliday/app/src/main/java/com/fatihkurcenli/myholiday/ui/fragment/AttractionDetailFragment.kt
@@ -1,10 +1,11 @@
 package com.fatihkurcenli.myholiday.ui.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.navigation.fragment.navArgs
+import com.fatihkurcenli.myholiday.R
 import com.fatihkurcenli.myholiday.data.Attraction
 import com.fatihkurcenli.myholiday.databinding.FragmentAttractionDetailBinding
 import com.squareup.picasso.Picasso
@@ -16,6 +17,11 @@ class AttractionDetailFragment : BaseFragment() {
     private val safeArgs: AttractionDetailFragmentArgs by navArgs()
     private val attraction: Attraction by lazy {
         attractions.find { it.id == safeArgs.attractionId }!!
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
@@ -36,6 +42,23 @@ class AttractionDetailFragment : BaseFragment() {
         binding.titleTextView.text = attraction.title
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_attraction_details, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menuItemLocation -> {
+                val gmnIntentUri =
+                    Uri.parse("geo:${attraction.location.latitude},${attraction.location.longitude}?z=9&q=${attraction.title}")
+                val mapIntent = Intent(Intent.ACTION_VIEW, gmnIntentUri)
+                startActivity(mapIntent)
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
     override fun onDestroy() {
         super.onDestroy()
